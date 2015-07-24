9fc634883ae4dd7d3ccf78002e80995d0f342525
Carlos Gochez <cgotcha@gmail.com>
2015-07-24 08:14:28 -0600

    Agregue clase de fecha

---
+++ src/TestDate.java
@@ -0,0 +1,18 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aula
+ */
+public class TestDate {
+    public static void main(String[] args) {
+        Date now = new Date();
+    }
+}
