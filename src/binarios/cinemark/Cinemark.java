43b8f0d004cd7fa672e36e4a1f524bd06013a3a5
Carlos Gochez <cgotcha@gmail.com>
2015-09-09 09:52:21 -0600

    avances en el sistema binario de cine

--- src/binarios/cinemark/Cinemark.java
+++ src/binarios/cinemark/Cinemark.java
@@ -18,6 +18,7 @@ public class Cinemark {
     private RandomAccessFile rp, rt;
     public static final String ROOT = "cinemark";
     public static final int PELICULA_OFFSET = 0, SALA_OFFSET=4, TICKET_OFFSET=8;
+    public static final int PRINT_ALL=0, PRINT_ACTIVE=1, PRINT_INACTIVE=2;
     
     public Cinemark(){
         try{
@@ -69,7 +70,64 @@ public class Cinemark {
     }
     
     public void printMovies(int all)throws IOException{
-        
+        rp.seek(0);
+        while(rp.getFilePointer() < rp.length()){
+            int cod = rp.readInt();
+            String no = rp.readUTF();
+            Date f = new Date(rp.readLong());
+            String r = rp.readUTF();
+            String t = rp.readUTF();
+            double d = rp.readDouble();
+            boolean a = rp.readBoolean();
+            
+            if(all==PRINT_ACTIVE && !a)
+                continue;
+            else if(all==PRINT_INACTIVE && a)
+                continue;
+            
+            System.out.println(cod+"-"+no+" Fecha: "+f+" Rating: "+r+
+                    " Tipo: "+t+"-"+d+" horas.");
+        }
+       
+    }
+    
+    private RandomAccessFile getSalaFile(int n)throws IOException{
+        return new RandomAccessFile(ROOT+"/sala"+n+".cnk", "rw");
+    }
+    
+    private boolean existsSala(int n){
+        String path = ROOT+"/sala"+n+".cnk";
+        return new File(path).exists();
+    } 
+    
+    public void addSala(TypeSala tipo, String horario, int cant)throws IOException{
+        int numero = nextCode(SALA_OFFSET);
+        try(RandomAccessFile rs = getSalaFile(numero)){
+            rs.writeInt(0);
+            rs.writeUTF(tipo.name());
+            rs.writeDouble(tipo.precio);
+            rs.writeUTF(horario);
+            rs.writeInt(cant);
+            for(int x=1; x <= cant; x++){
+                rs.writeBoolean(false);
+            }
+            
+        }
+    }
+    
+    public void printSalaInfo(int n)throws IOException{
+        if( existsSala(n) ){
+            try(RandomAccessFile rs = getSalaFile(n)){
+                //TODO: Una funcion de busqueda movie seria bueno
+                
+            }
+        }
+        else
+            System.out.println("Dicha Sala no Existe");
+    }
+    
+    public boolean assignMovieToSala(int cp, int ns)throws IOException{
+        return false;
     }
 
 }
